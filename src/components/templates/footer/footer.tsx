ab8949cf3a1e6a9b3036478f27fb4988bffae68e
Taurai. M <rusiqe@gmail.com>
2023-05-26 15:49:41 +0200

    powered

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -14,12 +14,12 @@ export const Footer = () => {
         <div className="mt-8">
           {t('footer.powerBy')}{' '}
           <Link
-            href="https://www.contentful.com"
+            href="https://twitter.com/rusIQe"
             rel="noopener noreferrer"
             target="_blank"
             className="text-blue500"
           >
-            Contentful
+            Tau
           </Link>
         </div>
       </Container>
